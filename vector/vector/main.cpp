1aeb98cb128306517bc2313561a6646d365a723b
ikduck <wogur2202@gmail.com>
2022-05-31 12:47:35 +0900

    Vector v0.5

--- vector/vector/main.cpp
+++ vector/vector/main.cpp
@@ -1,4 +1,4 @@
-// ***** Vector v0.4
+// ***** Vector v0.5
 #include <iostream>
 
 using namespace std;
@@ -19,10 +19,17 @@ int main(void)
 		{(char*)"FGHIJ"}
 	};
 	*/
-	
-	// git 새로운것을 덮어씌우지 않고 이전 버전을 지킬려고함	
-	// 
+
 
 
 	return 0;
 };
+
+// git 새로운것을 덮어씌우지 않고 이전 버전을 지킬려고함	
+// 브런치를 나눠서 구역을 나눔 , 경합하는 과정이 힘듦
+// Visual studio - IDE 코드 뿐만아니라 많은 편집기능을 포함하고있어 프로그램이 무거움
+// Visaul studio Mac - c컴파일할때 GCC라는 것이 필요했었다. 최근에는 모름
+// Visaul studio Code - 경량화 되어있음 컴파일러가없어 프로그램이 가볍다 
+// 전위 연산이 후위연산보다 더빠름 i++ < ++i 
+// 솔루션 탐색기 설정에서 버전바꾸는것 정도는 알아야함
+// 오늘 이후로는 git gui를 사용하는데 문제없이 만들것.
